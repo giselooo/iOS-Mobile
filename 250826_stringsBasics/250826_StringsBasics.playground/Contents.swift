efd7691bb4206ee2a0ffc233739b5c62345570ed
giselooo <giseldice6@gmail.com>
2026-08-26 16:00:22 -0600

    Ejercicio strings Basics del 25 de agosto de 2026

---
+++ 250826_stringsBasics/250826_StringsBasics.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let name = "Dana Reyes"
+
+let favoriteQuote = "Mi frase favorita es \"Veni, vidi, vici\""
+print(favoriteQuote)
+
+let emptyString = ""
+
+if emptyString.isEmpty{
+    print("There's nothigh here")
+}else{
+    print("It's not empty as I thought")
+}
