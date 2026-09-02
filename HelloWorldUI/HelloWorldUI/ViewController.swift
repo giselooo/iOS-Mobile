381d2beb74b188619db13516201b874e1a73a898
giselooo <giseldice6@gmail.com>
2026-09-01 19:54:29 -0600

    Ejercicio 3 del 1 de septiembre del 2026. Incluye HelloWorld y Captura de Pantalla

---
+++ HelloWorldUI/HelloWorldUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldUI
+//
+//  Created by Dana Gisel Reyes López on 01/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
